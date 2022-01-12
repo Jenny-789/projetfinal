2e8b3c7dd681317c1b48bc00d205e66fe82fdac2
Anthony <anthodov@hotmail.fr>
2022-01-12 09:30:09 +0100

    Page d'accueil html et css

--- src/app/inscription/inscription.component.ts
+++ src/app/inscription/inscription.component.ts
@@ -1,19 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-import { AbstractControl, FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormControl,
+} from '@angular/forms';
 import Validation from '../onglet-service/validation';
 import { Client } from '../model/client.model';
 import { ClientsService } from '../services/clients.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-
 @Component({
   selector: 'app-inscription',
   templateUrl: './inscription.component.html',
-  styleUrls: ['./inscription.component.css']
+  styleUrls: ['./inscription.component.css'],
 })
 export class InscriptionComponent implements OnInit {
-  newClient = new Client; 
-  message : string; 
+  newClient = new Client();
+  message: string;
 
   form: FormGroup = new FormGroup({
     title: new FormControl(''),
@@ -30,86 +35,86 @@ export class InscriptionComponent implements OnInit {
     acceptTerms: new FormControl(false),
   });
 
-    registerForm: FormGroup;
-    submitted = false;
+  registerForm: FormGroup;
+  submitted = false;
 
-
-   
-
-
-    constructor(private formBuilder: FormBuilder,private clientService : ClientsService, private http : HttpClient) { }
-    ngOnInit(): void {
-
-     
-
-      this.form = this.formBuilder.group(
-        { title: ['', Validators.required],
-          nom: ['', Validators.required],
-          prenom: ['', Validators.required],
-          birthday: ['', Validators.required],
-          adresse: ['', Validators.required],
-          codepostal: ['', Validators.required],
-          ville: ['', Validators.required],
-          username: [
-            '',
-            [
-              Validators.required,
-              Validators.minLength(6),
-              Validators.maxLength(20)
-            ]
+  constructor(
+    private formBuilder: FormBuilder,
+    private clientService: ClientsService,
+    private http: HttpClient
+  ) {}
+  ngOnInit(): void {
+    this.form = this.formBuilder.group(
+      {
+        title: ['', Validators.required],
+        nom: ['', Validators.required],
+        prenom: ['', Validators.required],
+        birthday: ['', Validators.required],
+        adresse: ['', Validators.required],
+        codepostal: ['', Validators.required],
+        ville: ['', Validators.required],
+        username: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(20),
           ],
-          email: ['', [Validators.required, Validators.email]],
-          password: [
-            '',
-            [
-              Validators.required,
-              Validators.minLength(6),
-              Validators.maxLength(40)
-            ]
+        ],
+        email: ['', [Validators.required, Validators.email]],
+        password: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(40),
           ],
-          confirmPassword: ['', Validators.required],
-          acceptTerms: [false, Validators.requiredTrue]
-        },
-        {
-          validators: [Validation.match('password', 'confirmPassword')]
-        }
-      );
-    }
-  
-  // Evite d'avoir de devoir noter tout le chemin dans le html, à la place j'y met juste f 
-    get f(): { [key: string]: AbstractControl } {
-      return this.form.controls;
-    }
-  
-    // Inscription
-    onSubmit(): void {
-      this.submitted = true;
-      //this.clientService.ajoutClient(this.newClient);
-      if (this.form.invalid) {
-        return;
-      }else { this.create();
-       }
-  
-      console.log(JSON.stringify(this.form.value, null, 2));
-      
-    }
-  
-    //Remise à zero 
-    onReset(): void {
-      this.submitted = false;
-      this.form.reset();
-    }
-
-    create(){
-      const body = JSON.stringify(this.newClient);
-      this.http.post("http://localhost:8080/formation/rest/personnes", body, { headers: new HttpHeaders({
-        "Content-Type": "application/json",}),
-        }).subscribe(response => {
-          console.log("Inscription réussi ")
-          this.message = "Inscription réussi - Bienvenue parmi nous " + this.newClient.prenom ;
-        });
-  
+        ],
+        confirmPassword: ['', Validators.required],
+        acceptTerms: [false, Validators.requiredTrue],
+      },
+      {
+        validators: [Validation.match('password', 'confirmPassword')],
       }
-    
+    );
   }
- 
+
+  // Evite d'avoir de devoir noter tout le chemin dans le html, à la place j'y met juste f
+  get f(): { [key: string]: AbstractControl } {
+    return this.form.controls;
+  }
+
+  // Inscription
+  onSubmit(): void {
+    this.submitted = true;
+    //this.clientService.ajoutClient(this.newClient);
+    if (this.form.invalid) {
+      return;
+    } else {
+      this.create();
+    }
+
+    console.log(JSON.stringify(this.form.value, null, 2));
+  }
+
+  //Remise à zero
+  onReset(): void {
+    this.submitted = false;
+    this.form.reset();
+  }
+
+  create() {
+    const body = JSON.stringify(this.newClient);
+    this.http
+      .post('http://localhost:8080/formation/rest/personnes', body, {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json',
+        }),
+      })
+      .subscribe((response) => {
+        console.log('Inscription réussi ');
+        this.message =
+          'Inscription réussi - Bienvenue parmi nous ' + this.newClient.prenom;
+      });
+  }
+}
