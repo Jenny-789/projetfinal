2b6a7f75144d96877be1a9b6fc9f8ac99c592664
Jenny <j.lambert789@gmail.com>
2022-01-12 22:15:23 +0100

    Newsletter

--- src/app/Footer/footer.component.ts
+++ src/app/Footer/footer.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Newsletter } from '../model/newsletter.model';
+import { FormBuilder, FormGroup, FormControl, AbstractControl } from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-footer',
@@ -7,9 +10,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  newsletter = new Newsletter();
+  message : String; 
+  submitted = false;
+
+  form: FormGroup = new FormGroup({
+    email: new FormControl('')
+  });
+
+  constructor(private formBuilder: FormBuilder, private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group(
+      {
+        email: [''],
+      });
+  }
+
+  get f(): { [key: string]: AbstractControl } {
+    return this.form.controls;
+  }
+  onSubmit(): void {
+    this.submitted = true;
+
+    if (this.form.invalid) {
+      return;
+    } else {
+      this.newslettersave();
+    }
+
+    console.log(JSON.stringify(this.form.value, null, 2));
+  }
+
+  newslettersave() {
+    const body = JSON.stringify(this.newsletter);
+    this.http.post("http://localhost:8080/formation/neswletter/email", body, {
+      headers: new HttpHeaders({
+        "Content-Type": "application/json",
+      }),
+    }).subscribe(response => {
+      console.log("email enregistré ")
+      this.message = "email enregistré " ;
+    });
+
   }
 
 }
